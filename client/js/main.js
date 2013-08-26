474ae239cff56580b4666213b5ad9516fbea806d
Gilbert Fritz <gilbert.fritz@gmx.at>
2013-08-26 16:47:08 +0200

    added name inpout

--- client/js/main.js
+++ client/js/main.js
@@ -2,8 +2,18 @@
 (function($) {
     $(function() {
 
-        var game = new Game();
-        var socketHandler = new SocketHandler('http://localhost:3000', game);
+        $('#gobutton').click(function() {
+            if ($('#nameinput').val().length >= 3) {
+                $('#textmessage').remove();
+                startGame();
+            } else {
+                alert("You name is too short!");
+            }
+        });
 
+        function startGame() {
+            var game = new Game();
+            var socketHandler = new SocketHandler('http://localhost:3000', game);
+        }
     });
 })(jQuery);
