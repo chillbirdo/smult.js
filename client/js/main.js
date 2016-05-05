74bdaa73a3083ceaa4854b33e679f97a8f2c5057
Gilbert <gilbert.fritz@gmx.at>
2016-05-05 23:59:39 +0200

    herokuurl

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-var SERVER = 'https://intense-caverns-57291.herokuapp.com/';
+var SERVER = 'https://young-stream-68847.herokuapp.com/';
 //var SERVER = 'http://localhost:3000';
 
 require(['HtmlHandler', 'Game', 'LocalInputReader', 'SocketHandler'],
