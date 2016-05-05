cb0e15cc3ff2954edac37be9497d57b086a61449
Gilbert <gilbert.fritz@gmx.at>
2016-05-05 23:51:11 +0200

    added express

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,5 @@
-var SERVER = 'https://young-stream-68847.herokuapp.com';
-//var SERVER = 'http://localhost:3000';
+//var SERVER = 'https://intense-caverns-57291.herokuapp.com/';
+var SERVER = 'http://localhost:3000';
 
 require(['HtmlHandler', 'Game', 'LocalInputReader', 'SocketHandler'],
         function(HtmlHandler, Game, LocalInputReader, SocketHandler) {
--- client/server.js
+++ client/server.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var app = express();
-
+var app = require('express')();
 var http = require('http').Server(app);
 
-app.use(express.static('client'));
-
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile('/home/gix/NetBeansProjects2016/smult.js/client/index.html');
 });
 
-var io = require('socket.io')(http);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+/*
+var io = require('socket.io').listen(process.env.PORT || 3000);
 
 var updatablePlayerInfos = {};
 var playerNames = {};
@@ -22,61 +23,58 @@ var playerNames = {};
 
 io.sockets.on('connection', function(client) {
 
-    console.log(">>>>> Client connected: " + cleanId(client.id));
+    console.log(">>>>> Client connected: " + client.id);
 
     client.emit('init_request', {playerNames: playerNames, updatablePlayerInfos: updatablePlayerInfos});
-    console.log(">>>>> emitted players to client " + cleanId(client.id));
+    console.log(">>>>> emitted players to client " + client.id);
 
-    client.broadcast.emit('log_message', {msg: 'new client connected: ' + cleanId(client.id)});
+    client.broadcast.emit('log_message', {msg: 'new client connected: ' + client.id});
     console.log(">>>>> emitted message to all clients except sender");
 
     // INIT_RESPONSE
     //////////////////
     client.on('init_response', function(data) {
-        if (!updatablePlayerInfos[cleanId(client.id)]) {
-            updatablePlayerInfos[cleanId(client.id)] = data.updatablePlayerInfo;
-            playerNames[cleanId(client.id)] = data.playerName;
+        console.log("HEHAHAHAHAHAHAH");
+        if (!updatablePlayerInfos[client.id]) {
+            updatablePlayerInfos[client.id] = data.updatablePlayerInfo;
+            playerNames[client.id] = data.playerName;
         } else {
             console.log("UNEXPECTED: wanted to add player that already existed!");
             return;
         }
         printUpdatablePlayerInfos(updatablePlayerInfos);
-        client.broadcast.emit('new_player_connected', {playerId: cleanId(client.id), updatablePlayerInfo: data.updatablePlayerInfo, playerName: data.playerName});
+        client.broadcast.emit('new_player_connected', {playerId: client.id, updatablePlayerInfo: data.updatablePlayerInfo, playerName: data.playerName});
     });
 
     // UPDATE_TO_SERVER
     /////////////////////
     client.on('update_to_server', function(data) {
-        data.updateInfo.id = cleanId(client.id);
-        updatablePlayerInfos[cleanId(client.id)] = data.updateInfo;
-        client.broadcast.emit('update_to_client', {playerId: cleanId(client.id), updateInfo: updatablePlayerInfos[cleanId(client.id)]});
+        data.updateInfo.id = client.id;
+        updatablePlayerInfos[client.id] = data.updateInfo;
+        client.broadcast.emit('update_to_client', {playerId: client.id, updateInfo: updatablePlayerInfos[client.id]});
 //        printUpdatablePlayerInfos(updatablePlayerInfos);
     });
 
     // CHAT_MESSAGE
     /////////////////////
     client.on('chat_message', function(data) {
-        client.broadcast.emit('chat_message', {playerId: cleanId(client.id), chatMessage: data.chatMessage});
+        client.broadcast.emit('chat_message', {playerId: client.id, chatMessage: data.chatMessage});
     });
 
 
     // DISCONNECT
     ///////////////
     client.on('disconnect', function() {
-        console.log(">>>>> diconnected: " + cleanId(client.id));
+        console.log(">>>>> diconnected: " + client.id);
 
-        delete updatablePlayerInfos[cleanId(client.id)];
-        delete playerNames[cleanId(client.id)];
+        delete updatablePlayerInfos[client.id];
+        delete playerNames[client.id];
         printUpdatablePlayerInfos(updatablePlayerInfos);
 
-        io.sockets.emit('player_disconnected', {id: cleanId(client.id)});
+        io.sockets.emit('player_disconnected', {id: client.id});
         console.log(">>>>> emitted player_disconnect to all clients");
     });
 });
-
-function cleanId(id){
-        return id.substring(2);
-}
 
 function printUpdatablePlayerInfos(updatablePlayerInfos) {
     var i = 0;
@@ -87,9 +85,4 @@ function printUpdatablePlayerInfos(updatablePlayerInfos) {
         console.log("name=" + name + "; id=" + id + "; posX=" + updatablePlayerInfo.posX + "; posY=" + updatablePlayerInfo.posY + "; direction=" + updatablePlayerInfo.direction + "; action=" + updatablePlayerInfo.activity + ";");
     }
 }
-
-
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
-});
-
+*/
